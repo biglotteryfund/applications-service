c239b7a9a34a963b8857668957e6977a9ae8823e
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-07-05 14:52:27 +0100

    Add date format helpers and linting

--- apps/dashboard/index.js
+++ apps/dashboard/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const logger = require("morgan");
 const nunjucks = require("nunjucks");
+const moment = require("moment");
 
 const dashboardRoute = require("./dashboard");
 
@@ -9,9 +10,33 @@ const app = express();
 
 app.use(logger("dev"));
 
+app.locals.DATE_FORMATS = {
+  short: "D MMMM, YYYY",
+  full: "dddd D MMMM YYYY",
+  fullTimestamp: "dddd D MMM YYYY (hh:mm a)"
+};
+
 const templateEnv = nunjucks.configure(".", {
   autoescape: true,
   express: app
+});
+
+/**
+ * View helper for formatting a date
+ * @param {String} dateString
+ * @param {String} format
+ * @see https://momentjs.com/docs/#/displaying/format/
+ */
+templateEnv.addFilter("formatDate", function(dateString, format) {
+  return moment(dateString).format(format);
+});
+
+/**
+ * View helper to represent date as relative time
+ * @param {String} dateString
+ */
+templateEnv.addFilter("timeFromNow", function(dateString) {
+  return moment(dateString).fromNow();
 });
 
 app.set("view engine", "njk").set("engineEnv", templateEnv);
